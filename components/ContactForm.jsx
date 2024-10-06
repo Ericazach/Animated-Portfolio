a34f3651e005508eb2b8ddb93c91dc7173b46bf0
Erica Sanchez <ericazach@gmail.com>
2024-10-06 18:57:06 +0200

    fix colors form

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -42,12 +42,12 @@ function ContactForm() {
           rows={6}
           name="user_message"
           id="user_message"
-          className="block py-4 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer resize-none"
+          className="block py-4 px-0 w-full text-sm text-neutral-200 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-neutral-200 dark:border-gray-400 dark:focus:neutral-200 focus:outline-none focus:ring-0 focus:border-neutral-200 peer resize-none"
           required
         />
         <label
           htmlFor="user_message"
-          className="peer-focus:font-medium absolute text-xl md:text-2xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          className="mb-4 peer-focus:font-medium absolute text-xl md:text-2xl text-neutral-200 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-neutral-200 peer-focus:dark:text-neutral-200 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           Dear Erica,
         </label>
@@ -57,24 +57,24 @@ function ContactForm() {
           type="email"
           name="user_email"
           id="user_email"
-          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-700 dark:focus:border-gray-700 focus:outline-none focus:ring-0 focus:border-neutral-200 peer"
           placeholder=" "
           required
         />
         <label
           htmlFor="user_email"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          className="peer-focus:font-medium absolute text-sm text-neutral-200 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-neutral-200 peer-focus:dark:text-neutral-200 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           My email address is
         </label>
       </div>
-      <p className="text-gray-500 dark:text-gray-400 text-sm md:text-lg py-4">
-        Regards
+      <p className="text-neutral-200 dark:text-gray-400 text-sm md:text-lg py-4">
+        Regards,
       </p>
 
       <button
         type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-white bg-secondary-100 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:hover:border-gray-600 border-gray-300 border dark:border-gray-600"
       >
         Submit
       </button>
--- components/ContactPage.jsx
+++ components/ContactPage.jsx
@@ -73,7 +73,7 @@ function ContactPage() {
           <button
             type="button"
             onClick={handleCopy}
-            className="mx-2 md:mx-20 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            className="mx-2 md:mx-20 text-neutral-200 bg-secondary-100 border border-gray-300 focus:outline-none hover:bg-gray-700 focus:ring-4 focus:ring-gray-100 font-bold rounded-lg text-md px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
           >
             {copied ? "Email is Copied!" : "Copy my Email"}
           </button>
