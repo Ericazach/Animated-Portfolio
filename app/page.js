b5c8fe35544b7d7d39fff9c7e75c0dcd6f50ff38
Erica Sanchez <ericazach@gmail.com>
2024-10-07 18:44:21 +0200

    change page

--- app/page.js
+++ app/page.js
@@ -8,15 +8,17 @@ import Social from "@/components/Social";
 
 export default function Home() {
   return (
-    <main className="dark:bg-gradient-to-b dark:from-[#1d3651] dark:to-[#531422] bg-gradient-to-b from-[#DD798F] to-[#29727C] flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 ">
-      <div className="w-full">
-        <Social />
-        <Navbar />
-        <Hero />
-        <About />
-        <Projects />
-        <ContactPage />
-      </div>
-    </main>
+    <div className="">
+      <main className="dark:bg-gradient-to-b dark:from-[#1d3651] dark:to-[#531422] bg-gradient-to-b from-[#DD798F] to-[#29727C] flex justify-center items-center flex-col  mx-auto sm:px-10 px-5">
+        <div className="w-full ">
+          <Social />
+          <Navbar />
+          <Hero />
+          <About />
+          <Projects />
+          <ContactPage />
+        </div>
+      </main>
+    </div>
   );
 }
